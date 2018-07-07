7d30983a00ba384081d69a389ecf605ba6c255f5
ashbhir <ashish.gambhir@grofers.com>
2018-07-07 21:54:37 +0530

    allow cors

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -2,6 +2,7 @@ export function handler(event, context, callback) {
   console.log(event)
   callback(null, {
     statusCode: 200,
+    headers: {'access-control-allow-origin': '*'},
     body: JSON.stringify({msg: "Hello, World!"})
   })
 }
